25af6a20bb6692e3bebaaba82c9fd4c0f2571054
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-04-01 10:13:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 var player = {
     health: 10,
-    maxHealth: 10
+    maxHealth: 10,
+    gold: 0,
+    level: 1,
+    xp: 0,
+    xpNeeded: 10
 }
+var gameLoop = setInterval("update()", 100);
+
+function update() {
+    if(xp >= xpNeeded) {
+        xp = 0;
+        xpNeeded = Math.round(xpNeeded * 1.25);
+        level++;
+        Console.apply("Level up! Level: " + level + " Xpneeded: " + xpNeeded);
+    }
+}
+
