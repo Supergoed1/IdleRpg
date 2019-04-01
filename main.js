62b9ac810542d2f8ba9de2b505e2dd0c98e3c9cf
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-04-01 10:24:12 +0200

    .

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ var player = {
     gold: 0,
     level: 1,
     xp: 0,
-    xpNeeded: 10
+    xpNeeded: 10,
+    multiplier: 1.30
 }
 var gameLoop = setInterval("update()", 100);
 
 function update() {
-    if(xp >= xpNeeded) {
-        xp = 0;
-        xpNeeded = Math.round(xpNeeded * 1.25);
-        level += 1;
-        Console.apply("Level up! Level: " + level + " Xpneeded: " + xpNeeded);
+    if(player.xp >= player.xpNeeded) {
+        player.xp = 0;
+        player.xpNeeded = Math.round(player.xpNeeded * player.multiplier);
+        player.level += 1;
+        console.log("Level up! Level: " + player.level + " Xpneeded: " + player.xpNeeded);
+        player.multiplier -= 0.001;
     }
-    xp += 1;
+    player.xp += 100000000000000000000;
 }
 
